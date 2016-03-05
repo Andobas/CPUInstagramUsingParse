c13a78ffee7daba4ddc5f5e8b9e08a4138727463
Andobas <one.e.toe@gmail.com>
2016-03-05 04:41:08 -0800

    Finished setup from video

--- CPUInstagramUsingParse/AppDelegate.swift
+++ CPUInstagramUsingParse/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "InstagramWithParse"
+                configuration.clientKey = "fqpwe0ane[w9n[ma[,fd'f'A"
+                configuration.server = "https://instagram-parse.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
